3722224440bfbf1c8bb39e71e6571bb324cf8db3
David Vilan Miguens <dvialnmiguens@danielcastelao.org>
2021-03-22 19:59:40 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Boletin12 {
+    public static void main(String[] args) {
+
+        Garaje g = new Garaje();
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Introduce numero de coches aparcados"));
+        g.setNumeroCoche(num);
+        if (num >= 0 && num < 5) {
+            System.out.println("PLAZAS DISPONIBLES");
+            g.setCoche(JOptionPane.showInputDialog("Introduce matricula"));
+            g.setHoras(Float.parseFloat(JOptionPane.showInputDialog("Introduce tiempo de estancia")));
+            g.precio2();
+            g.recivido(Float.parseFloat(JOptionPane.showInputDialog("Introduce dinero")));
+            g.factura();
+        } else if (num == 5) {
+            System.out.println("COMPLETO");
+
+        }
+
+    }
+
+}
